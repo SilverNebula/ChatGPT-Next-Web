ad6d8a0c0101865f0fd90ed6796c2931e7f0040d
SilverN <893325022@qq.com>
2024-05-15 20:44:33 +0800

    checkpoint

--- app/constant.ts
+++ app/constant.ts
@@ -8,7 +8,8 @@ export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/c
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
-export const DEFAULT_API_HOST = "https://api.nextchat.dev";
+// export const DEFAULT_API_HOST = "https://api.nextchat.dev";
+export const DEFAULT_API_HOST = "https://openkey.cloud";
 export const OPENAI_BASE_URL = "https://openkey.cloud";
 export const ANTHROPIC_BASE_URL = "https://api.anthropic.com";
 
